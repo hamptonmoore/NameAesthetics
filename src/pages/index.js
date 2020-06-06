4a81271db863d6ca718ea50f86c64a6ccf69d4df
Hampton Moore <me@hampton.pw>
2020-06-06 10:44:56 +0200

    wooooosh, it has entered the universea

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+
+
+export default () => (
+    <div className="grid grid-cols-5 h-full min-h-full max-h-full">
+                <div className="bg-cover bg-center col-span-3 h-auto" style={{backgroundImage: "url(https://picsum.photos/1000?1)"}}></div>
+                <div className="bg-cover bg-center col-span-2 h-auto" style={{backgroundImage: "url(https://picsum.photos/1000?2)"}}></div>
+                <div className="bg-cover bg-center col-span-2 h-auto" style={{backgroundImage: "url(https://picsum.photos/1000?3)"}}></div>
+                <div className="bg-cover bg-center col-span-3 h-auto" style={{backgroundImage: "url(https://picsum.photos/1000?4)"}}></div>
+    </div>
+    
+)

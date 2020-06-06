39f62e6cbfc0ca02fecafff7102465cd4d87136a
Hampton Moore <me@hampton.pw>
2020-06-06 13:40:42 +0200

    Span is name based

--- src/pages/[firstname]/[lastname].js
+++ src/pages/[firstname]/[lastname].js
@@ -1,14 +1,12 @@
 import {useRouter} from 'next/router'
-
+import {HashCode} from '../../modules/hashcode'
 import {Tile} from '../../component/tile'
 
 export default function Aesthetic() {
     const router = useRouter();
     let seed = `${router.query.firstname} ${router.query.lastname}`;
 
-    //let offset = 2 + Math.round(Math.random());
-        
-    let offset = 2;
+    let offset = 2 + (HashCode(seed) % 2)
 
     return(
         <div className="grid grid-cols-5 h-full min-h-full max-h-full">
